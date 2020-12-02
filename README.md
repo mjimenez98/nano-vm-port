d75beebbdeb307f0c95c1631e9f54ff1fba01c20
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-12-02 18:18:39 -0500

    Fix task 3
    - Update setup documentation
    - Fix CMakeLists
    - In _cout.c, fix baud rate, clean up, refactor
    - Fix task_3_runner.sh

--- README.md
+++ README.md
@@ -6,15 +6,31 @@ For CLion:
 1. Open or import nano_vm_port project
 2. Do `Run` > `Edit Configurations...`
   - Working directory: `~/nano-vm-port/tests/task_0`
-  - Program arguments (optional): Any test unit you want to run
-3. Build/run
+  - Program arguments (optional): Any test unit you want to run (e.g. T01.exe)
+3. Do `Preferences...` > `Build, Execution, Deployment` > `CMake`. Put `-DonTarget=0 -DDebugXtoa=0` in `CMake options`.
+`onTarget` indicates whether this run is on the host (computer) or on the target (ATmega328p). `DebugXtoa` prints debug
+strings from the target
+4. Build/run
 
 ## Running Tasks
 
 ### Tasks 0-1
 
-1. Open your terminal
+1. Open your terminal in `~/nano-vm-port`
 2. Run `cd tests/`
 3. Run `bash testRunner.sh`
 
 Now in your project structure you can go to `tests/task_0/output.txt` to find the output for all task 0 tests.
+
+### Task 3
+
+For this we will use two tabs in the terminal.
+
+1. Open your terminal in `~/nano-vm-port`
+2. Run `cd tasks/task_3`
+3. Compile task 3 on the target by running `bash task_3_runner.sh`
+4. Open a second tab in your terminal
+5. Run `ls /dev/tty.*` to know what port your chip is connected to
+6. Run `screen portName` where `portName` is the port being used by your chip. This will allow us to see the serial monitor
+
+You should now be able to see the output in your terminal
--- src/_cout.c
+++ src/_cout.c
@@ -5,7 +5,6 @@
 */
 
 #include "_outdesc.h"
-#include <stdio.h>
 
 #if ConsoleOutputWithPrintf
 
@@ -20,9 +19,6 @@ static void COut_PutN(void)          { printf("\n"); }
 #else
 #include "_xtoa.h"
 
-// External refs to 'console.c' without
-//void Console_Putchar(char  c);
-
 static char  buf[12];                /* to cover max size (12) "i32" (10+sign+null) */
 
 #ifdef onTarget
@@ -30,7 +26,7 @@ static char  buf[12];                /* to cover max size (12) "i32" (10+sign+nu
 #include <avr/io.h>
 
 #define FOSC 16000000
-#define BAUD 57600
+#define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
 
 static void COut_Init() {
@@ -45,11 +41,18 @@ static void COut_Init() {
     UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-void Console_Putchar(char c) { UDR0 = c; }
+void Console_Putchar(char c) {
+    // Wait until buffer is empty
+    while (!(UCSR0A & (1 << UDRE0)));
+
+    // Send char
+    UDR0 = c;
+}
 
 #else
 
-void Console_Putchar(char c) { putchar(c); }
+// External refs to 'console.c' without
+void Console_Putchar(char  c);
 
 #endif
 
@@ -61,6 +64,7 @@ static void COut_PutX(u32  x)        { System_utoa(x, buf, 16); COut_PutS(buf); 
                                                                                    // Hex alpha in upppercase
 static void COut_PutN(void)          { Console_Putchar('\n'); }
 static void COut_PutB(bool b)        { COut_PutS(b ? "true" : "false"); }
+
 #endif
 
 static IVMOutDesc cout = {
@@ -73,54 +77,37 @@ static IVMOutDesc cout = {
     COut_PutN
 };
 
-#ifdef onTarget
-
 static bool init = 0;
 
 IOut Out_GetFactory(const char* whichOne) {
     if (!init) {      // To make sure it will be done only once.
         whichOne = 0; // To avoid the warning on the unreferenced formal parameter.
+
+        #ifdef onTarget
         COut_Init();
+        #endif
+
         init = true;
     }
+
     return &cout;
 }
-
-#endif
 
 // --------------- Test unit ---------------
 
 #ifdef DebugXtoa
 
-#include <util/delay.h>
-#include <avr/io.h>
-
-#define MS_DELAY 1000
-
 static void TestCout(void) {
-//    COut_PutS("Test xtoa\n");
-//    COut_PutS("123456789ABCDEF\n");
-
-//    bsl_itoa(1234, buf);            COut_PutS(buf);
-//    bsl_utoa(56789, buf, 0, 10);    COut_PutS(buf);
-//    bsl_utoa(0xABCDEF, buf, 0, 16); COut_PutS(buf);
+    COut_PutS("Task 3 Test\n");
+    COut_PutS("123456789ABCDEF\n");
     COut_PutN();
 }
 
 int main (void) {
-//    COut_Init();        // Testing a direct call to init (as a first test).
-    Out_GetFactory("");   // Testing the factory that hides everything and does a lazy init.
-
-    // Set onboard LED for output. Just to see a blinking feedback from the Nano board.
-    DDRB |= (1 << PB5);
-
+    Out_GetFactory("");
     TestCout();
 
-    while(1) {
-        PORTB |= _BV(PORTB5);  // Turn the LED on.
-        _delay_ms(MS_DELAY);
-        PORTB &= ~_BV(PORTB5); // Turn the LED on.
-        _delay_ms(MS_DELAY);
-    }
+    return 0;
 }
+
 #endif
