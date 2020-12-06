623e879fcc30f55b3e03b2a303f4fb0794dc3f85
Mehdi Saidi <mehdisaidi879@gmail.com>
2020-12-06 18:30:59 -0500

    completed Task 7, task_7_runner.sh is still set to my port btw

--- tasks/task_7/bsl_IOReg.c
+++ tasks/task_7/bsl_IOReg.c
@@ -1,25 +1,16 @@
 #include "bsl_IOReg.h"
 
 #ifdef onTarget
-#include <avr.io/h>
+#include <avr/io.h>
 
 u32 bsl_IOReg_Read (u32 ioreg) {
-    // Implementation here
-    DDRB &= ~(0 << ioreg);
-
-    // Replace with actual value to return
-    return ((PINB & (1 << ioreg)))
+    // Return the contents of the register passed to the function
+     return (PINB & (1 << ioreg));
 }
 
 void bsl_IOReg_Write(u32 ioreg, u32 value) {
-    // Implementation here
-     DDRB = (1 << ioreg);
-
-     // set the output to the value given to the function
-     PORTB = (value << ioreg);
+    // Set the register to the value given to the function
+     *(volatile uint8_t *) ioreg = value;
 }
-
-
-
 #endif
 
--- tasks/task_7/task_7_runner.sh
+++ tasks/task_7/task_7_runner.sh
@@ -1,5 +1,5 @@
-avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -DonTarget bsl_TestIOReg0.c bsl_IOReg.c bsl_Uart.c hal_Loader.c ../../src/hal.c ../../src/out.c ../../src/vm.c ../../src/vmstack.c ../../src/_cout.c ../../src/_xtoa.c -o bsl_TestIOReg0.o
+avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -DonTarget bsl_TestIOReg0.c bsl_IOReg.c ../task_5/bsl_Uart.c ../../src/hal.c ../../src/out.c ../../src/vm.c ../../src/vmstack.c ../../src/_cout.c ../../src/_xtoa.c -o bsl_TestIOReg0.o
 
 avr-objcopy -O ihex -j .text -j .data bsl_TestIOReg0.o  bsl_TestIOReg0.hex
 
-avrdude -c arduino -p atmega328p -b 57600 -P /dev/tty.wchusbserial1410 -D -Uflash:w:bsl_TestIOReg0.hex:i
+avrdude -c arduino -p atmega328p -b 57600 -P /dev/tty.usbserial-1420 -D -Uflash:w:bsl_TestIOReg0.hex:i
