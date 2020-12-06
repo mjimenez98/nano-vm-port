73c204f10af4dd30c7a33c91e5334bb23cd1c9a7
Mehdi Saidi <mehdisaidi879@gmail.com>
2020-12-06 13:50:10 -0500

    implemented the BSL_IOreg.c file

--- tasks/task_7/bsl_IOReg.c
+++ tasks/task_7/bsl_IOReg.c
@@ -1,12 +1,25 @@
 #include "bsl_IOReg.h"
 
+#ifdef onTarget
+#include <avr.io/h>
+
 u32 bsl_IOReg_Read (u32 ioreg) {
     // Implementation here
+    DDRB &= ~(0 << ioreg);
 
     // Replace with actual value to return
-    return 1;
+    return ((PINB & (1 << ioreg)))
 }
 
 void bsl_IOReg_Write(u32 ioreg, u32 value) {
     // Implementation here
+     DDRB = (1 << ioreg);
+
+     // set the output to the value given to the function
+     PORTB = (value << ioreg);
 }
+
+
+
+#endif
+
